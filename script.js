5f5e53fa9dd7d1c4234b6df3486049528974d4b4
OsiLinux <osimayungbo@gmail.com>
2024-05-04 23:00:56 +0100

    Finish project

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-console.log("s")
-const container = document.getElementById("container");
-container.style.display = "flex";
+const refresh = document.getElementById("refresh");
 
-for (let column = 0; column < 16; column++) {
-    new_column = document.createElement("div");
-    new_column.setAttribute("id", "column_"+column);
-    new_column.style.flex =  1;
-    container.appendChild(new_column);
+run = function(size) {
+    const container = document.getElementById("container");
+    container.style.display = "flex";
+
+    for (let column = 0; column < size; column++) {
+        new_column = document.createElement("div");
+        new_column.setAttribute("id", "column_"+column);
+        new_column.style.flex =  1;
+        container.appendChild(new_column);
+        
     
-
-
-    for (let item = 0; item < 16; item++) {
-        div = document.createElement("div");
-        div.setAttribute("id", column);
-        div.textContent = column;
-        div.style.padding = "5px"
-        div.onmouseover = function() {
-            this.style.backgroundColor = "Red";
+    
+        for (let item = 0; item < size; item++) {
+            div = document.createElement("div");
+            div.setAttribute("id", column);
+            div.style.border = "1px solid black";
+            div.style.padding = "5px"
+            div.onmouseover = function() {
+                this.style.backgroundColor = "#"+Math.floor(Math.random()*16777215).toString(16);
+            }
+            new_column.appendChild(div);
         }
-        new_column.appendChild(div);
     }
 }
+
+run(16);
+
+refresh.onclick = function(){
+    let size = prompt("Enter grid size: ");
+    if (size > 100) {
+        size = 100;
+    }
+    const container = document.getElementById("container");
+    container.remove();
+    c = document.createElement("container");
+    c.setAttribute("id", "container");
+    document.getElementById("thebody").appendChild(c);
+    run(size);
+};
